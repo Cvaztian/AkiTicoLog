151bec05615b94d8a55dc56857c6dd3d5c3cbf75
Kevin <magneticok@gmail.com>
2020-05-27 19:09:59 -0600

    Database added
    First version of database file added

---
+++ database.pl
@@ -0,0 +1,98 @@
+%%% Hechos
+
+persona(mauricioHoffman).
+persona(victorCarvajal).
+persona(carlosRamos).
+persona('Keylor Navas').
+persona(joelCampbell).
+persona(claudiaPoll).
+persona(franklinChang).
+persona(ivanVargas).
+persona(clodomiroPicado).
+
+ocupacion(mauricioHoffman, presentador).
+ocupacion(victorCarvajal, presentador).
+ocupacion(carlosRamos, humorista).
+ocupacion(keylorNavas, futbolista).
+ocupacion(joelCampbell, futbolista).
+ocupacion(claudiaPoll, nadador).
+ocupacion(franklinChang, fisico).
+ocupacion(ivanVargas, fisico).
+ocupacion(franklinChang, ingeniero).
+ocupacion(franklinChang, astronauta).
+ocupacion(franklinChang, cientifico).
+ocupacion(clodomiroPicado, cientifico).
+ocupacion(ivanVargas, cientifico).
+
+afiliacion(mauricioHoffman, teletica).
+afiliacion(victorCarvajal, teletica).
+afiliacion(keylorNavas, realMadrid).
+afiliacion(keylorNavas, seleccionNacional).
+afiliacion(joelCampbell, seleccionNacional).
+afiliacion(joelCampbell, clubLeon).
+afiliacion(franklinChang, nASA).
+afiliacion(ivanVargas, tEC).
+afiliacion(clodomiroPicado, uCR ).
+afiliacion(kolbi, ICE).
+afiliación(gollo, Gollo).
+
+
+genero(mauricioHoffman, masculino).
+genero(victorCarvajal, masculino).
+genero(carlosRamos, masculino).
+genero(keylorNavas, masculino).
+genero(joelCampbell, masculino).
+genero(ivanVargas, masculino).
+genero(clodomiroPicado, masculino).
+genero(franklinChang, masculino).
+genero(claudiaPoll, femenino).
+
+reconocimientos(ivanVargas, ['Primer', descarga, de, plasma, en, latinoamerica]).
+reconocimientos(ivanVargas, ['Premio', nacional, de, ciencia, y, tecnologia]).
+reconocimientos(franklinChang, ['Desarrollo', de, motor, de, plasma]).
+reconocimientos(clodomiroPicado, ['Suero', antiofidico]).
+reconocimientos(clodomiroPicado, ['Benemerito', de, la, patria]).
+reconocimientos(clodomiroPicado, penicilina).
+
+extranjero(franklinChang).
+extranjero(clodomiroPicado).
+extranjero(claudiaPoll).
+
+altura(carlosRamos, pequeño).
+altura(keylornavas, pequeño).
+altura(claudiaPoll, alto).
+
+cabello(mauricioHoffman, corto).
+cabello(carlosRamos, corto).
+cabello(victorCarvajal, Copete).
+
+contextura(mauricioHoffman, musculoso).
+contextura(victorCarvajal, flaco).
+contextura(carlosRamos, grueso).
+
+tez(joelCampbell, afrodescendiente).
+tez(claudiaPoll, caucasica).
+
+tatuajes(mauricioHoffman).
+
+anteojos(mauricioHoffman).
+
+lgbtiq+(victorCarvajal).
+
+ojos(mauricioHoffman, claros).
+
+personalidad(carlosRamos, risueño).
+
+caricatura(kolbi).
+caricatura(gollo).
+caricatura('Tren de Teletica').
+
+clasificacion(kolbi, anfibio).
+
+clasificacion(gollo,ave).
+
+color(kolbi, verde).
+color(gollo, amarillo).
+color('Tren de Teletica', azul).
+
+clasificacion('Tren de Teletica', vehiculo).
