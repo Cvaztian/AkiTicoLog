d9a30887a860893497d9eae9688357b43f08fa22
GeraldSalazar <geraldsalazar4@gmail.com>
2020-05-29 23:42:32 -0600

    Add files via upload
    Se agregaron mas verbos y predicados.

--- Gramatica Libre De Contexto.pl
+++ Gramatica Libre De Contexto.pl
@@ -3,11 +3,10 @@ inputtolist:-
     read(Resp),nl,
     write(Resp),nl,
     split_string(Resp, " ", "", L),
-    oracion(L, []).
+    oracion(L, []), write("Acachete").
 
 
 oracion --> sujeto, verbo(G), predicado(G).
-
 
 sujeto --> [el].
 sujeto --> [ella].
@@ -17,6 +16,10 @@ verbo(a) --> [es].
 
 verbo(b) --> [trabaja].
 
+verbo(c) --> [tiene].
+
+
+
 
 predicado(a) --> [presentador].
 predicado(a) --> [humorista].
@@ -24,16 +27,37 @@ predicado(a) --> [futbolista].
 predicado(a) --> [nadador].
 predicado(a) --> [fisico].
 predicado(a) --> [ingeniero].
+
 predicado(a) --> [astronauta].
 predicado(a) --> [cientifico].
 
+predicado(a) --> [pequenno].
+predicado(a) --> [alto].
+
+predicado(a) --> [extranjero].
+
+predicado(a) --> [musculoso].
+predicado(a) --> [flaco].
+predicado(a) --> [grueso].
+predicado(a) --> [afrodescendiente].
+predicado(a) --> [cuacasica].
+
+predicado(a) --> [risuenno].
+
+predicado(a) --> [caricatura].
+
+
 predicado(b) --> [teletica].
+predicado(b) --> [nASA].
+predicado(b) --> [tEC].
+predicado(b) --> [uCR].
 
 
 
 
-
-
+predicado(c) --> [pelocorto].
+predicado(c) --> [tatuajes].
+predicado(c) --> [anteojos].
 
 
 
