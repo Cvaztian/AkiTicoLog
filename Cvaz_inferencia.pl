97a8d95b7a0802de0d8278f697b88ea62752d34f
Cvaz <scyock.03@gmail.com>
2020-05-31 02:33:01 -0600

    Input descompuesto

---
+++ Cvaz_inferencia.pl
@@ -0,0 +1,54 @@
+%Positivos
+positivo('Si').
+positivo('si').
+
+%Negativos
+negativo('No').
+negativo('no').
+
+%Omitibles
+omitir('el').
+omitir('la').
+omitir('lo').
+omitir('y').
+omitir('de').
+omitir('a').
+omitir('un').
+omitir('una').
+omitir('uno').
+omitir('en').
+omitir('para').
+
+
+%_________________________________%
+%Input de usuario binario u oracion
+%*********************************%
+answer:-
+  write("Por favor ingrese su respuesta"),nl,
+  read(Resp),nl,
+  %positivo(Resp), true;
+  %negativo(Resp), !, false;
+  write(Resp), inputtolist(Resp, List), write(List).
+
+
+%_________________________________%
+%Input de usuario (string) a lista
+%*********************************%
+inputtolist(Resp, L):- split_string(Resp, " ", "", L).
+
+%_______________________________%
+%Lista sin palabras innecesarias
+%*******************************%
+purgar([], Result, Result).
+purgar([X|Y], Lista, Result):-not(omitir(X)), (add(Lista, X, NuevLista), write(NuevLista), purgar(Y, NuevLista, Result)); purgar(Y, Lista, Result).
+purgar(Lista, Result):- purgar(Lista, [], Result).
+
+%_______________________________%
+%Add list to list
+%*******************************%
+add([], Elem, [Elem]).
+add(List, Elem, [List|Elem]).
+%append([X|Y], Z, [X|W]):- append(Y, Z, W).
+
+
+%play:- purgar(X), prueba(X).
