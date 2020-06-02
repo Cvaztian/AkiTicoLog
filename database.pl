9a796347bf44410d4e7499eca76c476782f5b77e
Kevin <magneticok@gmail.com>
2020-06-01 20:02:07 -0600

    Reestructuracion del algoritmo general de solucion

--- database.pl
+++ database.pl
@@ -101,11 +101,120 @@ color('Rana de Kolbi', verde).
 color('Gallo Gollo', amarillo).
 color('Tren de Teletica', azul).
 
+personaje('Gallo gollo',[nao,f,gollo,f,amarillo,f]).
+personaje('Rana de Kolbi',[nao,f,kolbi,f,verde,f]).
+personaje('Tren de Teletica',[nao,f,teletica,azul,f]).
+personaje('Joel Campbell',[si,futbolista,clubLeon,f,f,f]).
+personaje('Keylor Navas',[si,futbolista,realMadrid,f,f,f]).
+personaje('Mauricio Hoffman',[si,presentador,teletica,masculino,f,si]).
+personaje('Victor Carvajal',[si, presentador, teletica, masculino, f,nao]).
+personaje('Clodomiro Picado',[si, cientifico, uCR, f, f,f]).
+personaje('Ivan Vargas', [si, cientifico, tEC, f, f,f]).
+personaje('Frankling Chang',[si, cientifico, nASA, f, f,f]).
+personaje('Pilar Cisneros',[si, presentador, teletica, femenino, f,nao]).
+personaje('Claudia Poll',[si, nadador, f, femenino, f, f]).
+personaje('Carlos Ramos',[si, humorista, f, masculino, f, f]).
 
 
 validez('y').
 
+% Colores validos
+valid(color, amarillo).
+valid(color, azul).
+valid(color, verde).
+
+% Ocupaciones validas
+valid(ocupacion, presentador).
+valid(ocupacion, futbolista).
+valid(ocupacion, nadador).
+valid(ocupacion, humorista).
+valid(ocupacion, cientifico).
+
+% Afiliaciones validas
+valid(afiliacion, teletica).
+valid(afiliacion, realMadrid).
+valid(afiliacion, clubLeon).
+valid(afiliacion, nASA).
+valid(afiliacion, tEC).
+valid(afiliacion, uCR).
+valid(afiliacion, gollo).
+valid(afiliacion, kolbi).
+
+% Generos validos
+valid(genero, masculino).
+valid(genero, femenino).
+
+% Real validos
+valid(real, si).
+valid(real, nao).
+
+% Lentes validos
+valid(lentes, si).
+valid(lentes, nao).
+
 %%% Reglas
+
+orch([Real,Ocup,Afil,Gen,Col,Len]):- % Funcion orquestadora
+  verificar_pj([Real,Ocup,Afil,Gen,Col,Len]),
+  q_anim([Real|Ocup|Afil|Gen|Col]);
+  q_ocup([Real|Ocup|Afil|Gen|Col]);
+  q_afil([Real|Ocup|Afil|Gen|Col]);
+  q_gen([Real|Ocup|Afil|Gen|Col]);
+  q_col([Real|Ocup|Afil|Gen|Col]);
+  valid_all([Real|Ocup|Afil|Gen|Col]).
+
+verificar_pj([Real,Ocup,Afil,Gen,Col,Len]):-
+  Real==nao,valid(color,Col),valid(afiliacion, Afil);
+  Ocup==cientifico, valid(afiliacion, Afil);
+  Ocup==presentador, valid(genero, Gen), valid(lentes, Lentes);
+  Ocup==futbolista, valid(afiliacion, Afil);
+  Ocup==nadador;
+  Ocup==humorista.
+
+q_anim([Real,Ocup,Afil,Gen,Col,Len]):-
+  valid(real, Real);
+  BNF(animacion),
+  orch([Real,Ocup,Afil,Gen,Col,Len]).
+
+q_ocup([Real,Ocup,Afil,Gen,Col,Len]):-
+  valid(ocupacion, Ocup);
+  Real==nao;
+  BNF(ocupacion), orch([Real,Ocup,Afil,Gen,Col,Len]).
+
+% Preguntas de afiliacion
+q_afil([Real,Ocup,Afil,Gen,Col,Len]):-
+  valid(afiliacion, Afil);
+  Real==nao, q_afil_anim([Real,Ocup,Afil,Gen,Col,Len]), orch([Real,Ocup,Afil,Gen,Col,Len]);
+  Ocup==cientifico, q_afil_cient([Real,Ocup,Afil,Gen,Col,Len]), orch([Real,Ocup,Afil,Gen,Col,Len]);
+  Ocup==futbolista, q_afil_futbol([Real,Ocup,Afil,Gen,Col,Len]), orch([Real,Ocup,Afil,Gen,Col,Len]);
+  Ocup==presentador;Ocup==nadador;Ocup==humorista.
+
+q_afil_cient([Real,Ocup,Afil,Gen,Col,Len]):-
+  BNF(afil_cint).
+
+q_afil_anim([Real,Ocup,Afil,Gen,Col,Len]):-
+  BNF(afil_anim).
+
+q_afil_futbol([Real,Ocup,Afil,Gen,Col,Len]):-
+  BNF(afil_fut).
+
+q_genero([Real,Ocup,Afil,Gen,Col,Len]):-
+  valid(genero, Gen);
+  not(Ocup==presentador);
+  BNF(genero), orch([Real,Ocup,Afil,Gen,Col,Len]).
+
+q_color([Real,Ocup,Afil,Gen,Col,Len]):-
+  valid(color, Col);
+  not(Real==nao);
+  BNF(color), orch([Real, Ocup, Afil, Gen, Col, Len]).
+
+q_lentes([Real,Ocup,Afil,Gen,Col,Len]):-
+  valid(lentes, Len);
+  not(Ocup==presentador);
+  BNF(lentes), orch([Real,Ocup,Afil,Gen,Col,Len]).
+
+
+
 
 play:-
   question_animated;
