ce0b19dc2888180fd732e5c8cd182982be06a691
Cvaz <scyock.03@gmail.com>
2020-06-03 02:50:20 -0600

    Regla para antonimos
    Se incluye la regla para determinar el valor de una oracion negativa y se hicieron mejoras al BNF para mayor inclusividad del lenguaje natural

--- Cvaz_inferencia.pl
+++ Cvaz_inferencia.pl
@@ -7,47 +7,65 @@ oracion --> negacion.                                                 %Oracion b
 oracion --> sintagma_nominal, sintagma_verbal.                        %Oracion completa
 sintagma_nominal --> pronombre.                                       %forma: "ella"
 sintagma_nominal --> negacion, pronombre.                             %forma: "no, ella"
+sintagma_nominal --> adj_pos, sujeto.                                 %forma: "mi personaje"
 sintagma_verbal --> verbo(G), predicado(G).                           %forma: "es flaco"
+sintagma_verbal --> verbo(G), articulo_indef, predicado(G).        %forma: "es una caricatura"
 sintagma_verbal --> verbo(G), preposicion, predicado(G).              %forma: "trabaja en teletica"
-sintagma_verbal --> verbo(G), preposicion, articulo, predicado(G).    %forma "trabaja para la nasa"
+sintagma_verbal --> verbo(G), preposicion, articulo_def, predicado(G).%forma "trabaja para la nasa"
+%Antonimos (caso especial de oracion)                                 %Esta estructura permite determinar el valor contrario a lo que el usuario niega
+antonimo --> sintagma_nominal, sintagma_verbal_neg.                   %Oracion con una negacion
+sintagma_verbal_neg --> negacion, sintagma_verbal.                    %forma: "no es real"
 
 %Afirmaciones
 afirmacion --> [si].
 
 %Negaciones
 negacion --> [no].
-negacion_hecho(no).
 
 %Pronombres
 pronombre --> [el].
 pronombre --> [ella].
 
+%sujeto
+sujeto --> [personaje].
+
 %Articulos
-articulo --> [la].
-articulo --> [el].
+articulo_def --> [la].
+articulo_def --> [el].
+articulo_indef --> [un].
+articulo_indef --> [una].
+
+%adjetivo posesivo
+adj_pos --> [mi].
 
 %preposiciones
 preposicion --> [para].
 preposicion --> [en].
+preposicion --> [de].
 
 %Verbo ser (tipo a)
 verbo(a) --> [es].
 %Verbo trabajar (tipo b)
 verbo(b) --> [trabaja].
 %Verbo tener (tipo c)
+verbo(c) --> [lleva].
 verbo(c) --> [tiene].
+verbo(c) --> [usa].
 
 %Predicados para verbo ser (tipo a)
 predicado(a) --> [presentador].
+predicado(a) --> [presentadora].
 predicado(a) --> [humorista].
 predicado(a) --> [futbolista].
 predicado(a) --> [nadador].
+predicado(a) --> [nadadora].
 predicado(a) --> [fisico].
 predicado(a) --> [ingeniero].
 predicado(a) --> [astronauta].
 predicado(a) --> [cientifico].
 
 predicado(a) --> [extranjero].
+predicado(a) --> [ucr].
 
 predicado(a) --> [musculoso].
 predicado(a) --> [flaco].
@@ -82,13 +100,23 @@ predicado(c) --> [tatuajes].
 predicado(c) --> [anteojos].
 predicado(c) --> [lentes].
 
+%Antonimos
+%args: valor, valor contrario
+contrario(real, caricatura).
+contrario(caricatura, real).
+contrario(hombre, mujer).
+contrario(mujer, hombre).
+contrario(lentes, nao).
+
 %_________________________________%
 %Input de usuario binario u oracion
 %*********************************%
 answer(Valor):-
   write("Por favor ingrese su respuesta"),nl,
   read(Resp),nl,
-  inputtolist(Resp, Liststr), atom_list(Liststr, Listatom), oracion(Listatom, []), gener_vals(Listatom, Valor);
+  inputtolist(Resp, Liststr), atom_list(Liststr, Listatom),
+  antonimo(Listatom, []), gener_vals_cons(Listatom, Valor);
+  oracion(Listatom, []), gener_vals(Listatom, Valor);
   write("La estructura de su respuesta es incorrecta."), nl, write("Intente nuevamente"), nl, answer(Valor).
 
 %______________________________________________%
@@ -104,43 +132,16 @@ inputtolist(Resp, L):- split_string(Resp, " ", ",", L).
 atom_list([], []).
 atom_list([Hstr|Tstr], [Hatom|Tatom]):- atom_string(Hatom, Hstr), atom_list(Tstr, Tatom).
 
-%______________________________%
-%Determinar signo de la oracion
-%args: lista de atomos
-%******************************%
-negacion_regla([]):- fail.
-negacion_regla([Head|Tail]):-
-  negacion_hecho(Head), true;
-  negacion_regla(Tail).
-
 %______________________________________________%
 %Generar valores
 %args: oracion como lista de atomos
 %**********************************************%
 gener_vals([Ultimo], [Ultimo]).
-gener_vals([Head|Tail], Resp):- gener_vals(Tail, Resp).
+gener_vals([_|Tail], Resp):- gener_vals(Tail, Resp).
 
-%_______________________________%
-%Lista sin palabras innecesarias
-%*******************************%
-%purgar([], Result, Result).
-%purgar([X|Y], Lista, Result):-not(omitir(X)), (add(Lista, X, NuevLista), purgar(Y, NuevLista, Result)); purgar(Y, Lista, Result).
-%purgar(Lista, Result):- purgar(Lista, [], Result).
-
-%_______________________________%
-%Add list to list
-%*******************************%
-add([], Elem, Elem).
-add(List, Elem, [List|Elem]).
-
-%_________________________________%
-%Determinar si existe negacion
-%*********************************%
-%negacion([]):-false.
-%negacion([X|Y]):- negativo(X), true; negacion(Y).
-
-%_________________________________%
-%Identificar si coinciden palabras
-%*********************************%
-%coincide([], _):- false.
-%coincide([X|Y], Palabra):- X==Palabra, true; coincide(Y, Palabra).
+%______________________________________________%
+%Generar valores contrarios
+%args: oracion como lista de atomos
+%**********************************************%
+gener_vals_cons([Ultimo], Anton):- contrario(Ultimo, Anton).
+gener_vals_cons([_|Tail], Resp):- gener_vals_cons(Tail, Resp).
